45d12086ed1f88aa53b3319f6dc40c44008a2491
kyu1111 <pkb121259@gmal.com>
2023-12-01 16:10:18 +0900

    v3.0

---
+++ src/myproject/main.java
@@ -0,0 +1,11 @@
+package myproject;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+		System.out.print("java");
+	}
+
+}
